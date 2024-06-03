824854254d732b2bc137cb05ac06a4d107e7ce57
localhostov <localhostov@gmail.com>
2024-06-03 19:09:55 +0300

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/me/localx/xtype/MainActivity.kt
@@ -0,0 +1,37 @@
+package me.localx.xtype
+
+import App
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                scrim = Color.Transparent.toArgb(),
+            ),
+            navigationBarStyle = SystemBarStyle.dark(
+                scrim = Color.Transparent.toArgb(),
+            )
+        )
+
+        setContent {
+            App()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
